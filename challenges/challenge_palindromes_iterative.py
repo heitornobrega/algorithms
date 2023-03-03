479a0a9aecf0813fcbaacd4a9aabee8bb81bdee3
heitornobrega <nobheitor@gmail.com>
2023-03-03 06:31:25 -0300

    é o trem

--- challenges/challenge_palindromes_iterative.py
+++ challenges/challenge_palindromes_iterative.py
@@ -1,3 +1,8 @@
 def is_palindrome_iterative(word):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    if len(word) == 0:
+        return False
+    word = word.lower()
+    for i in range(len(word) // 2):
+        if word[i] != word[-i - 1]:
+            return False
+    return True
