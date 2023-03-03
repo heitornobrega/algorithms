6fac7eeb172705af9abae35dcaae1fb9f5efa8de
heitornobrega <nobheitor@gmail.com>
2023-03-02 22:41:21 -0300

    feat: study_schedule function

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,3 +1,24 @@
+def validate_input(permanence_period, target_time):
+    if not isinstance(permanence_period, list):
+        return False
+    if not all(
+        isinstance(tupla, tuple) and all(isinstance(i, int) for i in tupla)
+        for tupla in permanence_period
+    ):
+        return False
+    if not isinstance(target_time, int):
+        return False
+    return True
+
+
 def study_schedule(permanence_period, target_time):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    if not validate_input(permanence_period, target_time):
+        return None
+    c = 0
+    for student_time in permanence_period:
+        if student_time[0] <= target_time <= student_time[1]:
+            c += 1
+    return c
+
+
+print(study_schedule([(2, 2), (1, 2), (2, 3), (1, 5), (4, 5), (4, 5)], 1))
