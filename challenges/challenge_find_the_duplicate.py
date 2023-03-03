3b1f7be3380970dcf396c59380af58839b6ebe67
heitornobrega <nobheitor@gmail.com>
2023-03-03 05:55:37 -0300

    feat: implementation of find_duplicate

--- challenges/challenge_find_the_duplicate.py
+++ challenges/challenge_find_the_duplicate.py
@@ -1,3 +1,10 @@
 def find_duplicate(nums):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    if not all(isinstance(num, int) and num >= 0 for num in nums):
+        return False
+
+    seen = set()
+    for num in nums:
+        if num in seen:
+            return num
+        seen.add(num)
+    return False
